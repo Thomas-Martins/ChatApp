c34fb83dd71510b41d303d68c02e1c7113212117
Thomas Martins <thomas.martins1710@gmail.com>
2024-01-16 16:25:08 +0100

    chore: create and save token when login or register

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,6 @@ Route::middleware('auth:sanctum')->group(function () {
   Route::post('/logout', [AuthController::class,'logout']);
 });
 //Authentication routes
-Route::post('/login', [AuthController::class,'login']);
 Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
 
