a07fdd277b2586585d2339bed0303f85594ca611
Thomas Martins <thomas.martins1710@gmail.com>
2024-02-07 14:15:52 +0100

    Add Modal in Sidebar for send message to a friend

--- react-front/src/components/Sidebar.jsx
+++ react-front/src/components/Sidebar.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TiUserAddOutline } from "react-icons/ti";
 import { useAuth } from "../contexts/AuthProvider";
+import { getAllFriends } from "../utils/friendUtils";
 import UserModal from "./modal/UserModal";
 
 export default function Sidebar() {
   const { userData, token, setUserData, setToken } = useAuth();
   const [showModal, setShowModal] = useState(false);
   const [showMessageModal, setShowMessageModal] = useState(false);
+  const [friendList, setFriendList] = useState([]);
+  const [selectedFriendId, setSelectedFriendId] = useState(null);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    getAllFriends(token, setFriendList);
+    document.addEventListener("mousedown", handleClickOutsideModal);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideModal);
+    };
+  }, [token]);
 
   // Function for getting the first letter of the username
   const getInitials = (username) => {
@@ -25,7 +37,6 @@ export default function Sidebar() {
 
   const onLogout = async (e) => {
     e.preventDefault();
-
     await fetch(`${import.meta.env.VITE_API_BASE_URL}/logout`, {
       method: "POST",
       headers: {
@@ -50,6 +61,17 @@ export default function Sidebar() {
         console.error(error);
       });
   };
+
+  const handleClickOutsideModal = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      setShowMessageModal(false);
+    }
+  };
+
+  const handleFriendSelection = (friendId) => {
+    setSelectedFriendId(friendId);
+  };
+
   return (
     <div className="flex h-screen min-w-52 w-52 flex-col justify-between bg-gray-800">
       <div className="px-4 py-6">
@@ -63,26 +85,82 @@ export default function Sidebar() {
         </div>
 
         <div>
-          <div className="flex justify-between items-center relative bg-green-500">
+          <div className="flex justify-between items-center relative">
             <p className="text-gray-500 text-sm">MESSAGES</p>
             <button onClick={() => setShowMessageModal(!showMessageModal)}>
               <span className="text-gray-500 hover:text-white">+</span>
             </button>
             {showMessageModal && (
-              <div className="absolute bg-red-600 text-white top-10">
-                <div className="w-[350px] h-[350px] rounded-lg">
+              <div
+                ref={modalRef}
+                className="absolute z-50 -left-6 translate-x-1/2 top-10 bg-gray-700 text-white border-[1px] border-gray-800 rounded-lg shadow-2xl p-4"
+              >
+                <div className="relative w-[350px] h-[350px] rounded-lg">
                   {/*content*/}
-                  <div className="bg-gray-700 border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
-                    {/*header*/}
-                    <div className="w-full h-[50px] bg-indigo-400 relative rounded-t-lg">
-                      <a
-                        to="/edit-user"
-                        className="absolute text-white right-2 top-2 bg-indigo-600 p-2 rounded-full hover:bg-indigo-800"
-                      >
-                        x
-                      </a>
+                  <div className="">
+                    <div>
+                      <h1 className="font-semibold px-3 mb-3">
+                        Choisis une personne avec qui parler.
+                      </h1>
                     </div>
+                    {Array.isArray(friendList) && friendList.length > 0 ? (
+                      <fieldset className="space-y-4">
+                        <legend className="sr-only">Friends</legend>
+                        {friendList.map((friends) => {
+                          // Vérifier si l'utilisateur connecté est le sender ou le receiver
+                          const friend =
+                            friends.sender.id !== userData.id
+                              ? friends.sender
+                              : friends.receiver;
+                          return (
+                            <div
+                              key={friend.id}
+                              className="flex justify-between items-center hover:bg-gray-600 px-3 rounded-lg transition-all cursor-pointer"
+                            >
+                              <label
+                                htmlFor={`friendCheckbox_${friend.id}`}
+                                className="flex items-center w-full"
+                              >
+                                <div className="flex items-center w-full py-3 gap-3">
+                                  <div className="grid h-12 w-14 place-content-center rounded-full border-white bg-indigo-400">
+                                    <img
+                                      src="../src/assets/images/logo/logo.png"
+                                      alt="Profile"
+                                    />
+                                  </div>
+                                  <div className="w-full">
+                                    <p className="text-white font-semibold">
+                                      {friend.username}
+                                    </p>
+                                  </div>
+                                </div>
+                                <input
+                                  type="radio"
+                                  id={`friendCheckbox_${friend.id}`}
+                                  name="selectedFriend"
+                                  className="size-5 border-gray-300 checked:accent-indigo-500"
+                                  value={friend.id}
+                                  checked={selectedFriendId === friend.id}
+                                  onChange={() =>
+                                    handleFriendSelection(friend.id)
+                                  }
+                                />
+                              </label>
+                            </div>
+                          );
+                        })}
+                      </fieldset>
+                    ) : (
+                      <div>
+                        <p className="text-gray-400">
+                          Tu n&apos;as pas encore d&apos;amis.
+                        </p>
+                      </div>
+                    )}
                   </div>
+                </div>
+                <div className="bg-indigo-500 p-3 text-center rounded-lg shadow-lg hover:bg-indigo-600 duration-300">
+                  <button>Créer un MP</button>
                 </div>
               </div>
             )}
@@ -151,7 +229,13 @@ export default function Sidebar() {
           </span>
         </button>
       </div>
-      {showModal && <UserModal user={userData} setShowModal={setShowModal} />}
+      {showModal && (
+        <UserModal
+          user={userData}
+          setShowModal={setShowModal}
+          modalRef={modalRef}
+        />
+      )}
     </div>
   );
 }
